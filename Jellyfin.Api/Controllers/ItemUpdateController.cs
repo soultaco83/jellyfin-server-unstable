6238448716a23cc1277c552da1e144af4a434c81
theguymadmax <theguymadmax@proton.me>
2026-07-13 23:08:06 -0400

    Update season and episode SeriesName when renaming a series

--- Jellyfin.Api/Controllers/ItemUpdateController.cs
+++ Jellyfin.Api/Controllers/ItemUpdateController.cs
@@ -297,6 +297,8 @@ public class ItemUpdateController : BaseJellyfinApiController
         {
             foreach (var season in rseries.Children.OfType<Season>())
             {
+                season.SeriesName = rseries.Name;
+
                 if (!season.LockedFields.Contains(MetadataField.OfficialRating))
                 {
                     season.OfficialRating = request.OfficialRating;
@@ -314,6 +316,8 @@ public class ItemUpdateController : BaseJellyfinApiController
 
                 foreach (var ep in season.Children.OfType<Episode>())
                 {
+                    ep.SeriesName = rseries.Name;
+
                     if (!ep.LockedFields.Contains(MetadataField.OfficialRating))
                     {
                         ep.OfficialRating = request.OfficialRating;
